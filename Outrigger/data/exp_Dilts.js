26c0a6efcd43ef5319ecc75f98835fecf5d44656
Mike <mikejones.geo@gmail.com>
2015-03-06 11:07:57 -0700

    test

---
+++ Outrigger/data/exp_Dilts.js
@@ -0,0 +1,8 @@
+var exp_Dilts = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "color_qgis2leaf": '#97315a', "radius_qgis2leaf": 1.3, "transp_qgis2leaf": 1.0, "transp_fill_qgis2leaf": 1.0, "Name": "Dilts #1", "Descriptio": "<style>body { font-family:Arial; font-size:11px } h1 { font-family:Arial; font-size:20px; } h2 { font-family:Arial; font-size:16px; } h3 { font-family:Arial; font-size:12px; } h4 { font-family:Arial; font-size:11px; } h5 { font-family:Arial; font-size:8p" }, "geometry": { "type": "LineString", "coordinates": [ [ -105.57577652606402, 43.348980491559502, 0.0 ], [ -105.576916605107, 43.347374306844102, 0.0 ], [ -105.574549566493, 43.346827707464797, 0.0 ], [ -105.57345090123201, 43.348509618314708, 0.0 ], [ -105.57577652606402, 43.348980491559502, 0.0 ] ] } }
+]
+}
