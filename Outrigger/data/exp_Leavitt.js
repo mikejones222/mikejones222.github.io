f016867528d0851909b385c80a97d5c1eece00bf
Mike <mikejones.geo@gmail.com>
2015-03-25 08:29:12 -0600

    Delete

--- Outrigger/data/exp_Leavitt.js
+++
@@ -1,8 +0,0 @@
-var exp_Leavitt = {
-"type": "FeatureCollection",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-                                                                                
-"features": [
-{ "type": "Feature", "properties": { "color_qgis2leaf": '#0f5682', "radius_qgis2leaf": 1.3, "transp_qgis2leaf": 1.0, "transp_fill_qgis2leaf": 1.0, "Name": "Leavitt #1", "Descriptio": "<style>body { font-family:Arial; font-size:11px } h1 { font-family:Arial; font-size:20px; } h2 { font-family:Arial; font-size:16px; } h3 { font-family:Arial; font-size:12px; } h4 { font-family:Arial; font-size:11px; } h5 { font-family:Arial; font-size:8p" }, "geometry": { "type": "LineString", "coordinates": [ [ -105.64379441348299, 43.648683108284402, 0.0 ], [ -105.643801134074, 43.646903658109999, 0.0 ], [ -105.64119401256799, 43.6468871456252, 0.0 ], [ -105.641127788802, 43.648690268297699, 0.0 ], [ -105.64379441348299, 43.648683108284402, 0.0 ] ] } }
-]
-}
